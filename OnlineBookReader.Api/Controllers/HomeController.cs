e154bb621831c57cee0251b22d87b4076e19bf75
minh.chau <minh.chau@bmbsoft.com.vn>
2025-04-25 17:53:22 +0700

    first commit

---
+++ OnlineBookReader.Api/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace OnlineBookReader.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+    }
+}
